7bd16377e3c06b4fe8763234692e022455253684
Amit Joshi <amit.joshi@stellarinfo.com>
2022-08-16 12:15:09 +0530

    Second Phase

--- routes/invoice.js
+++ routes/invoice.js
@@ -7,6 +7,7 @@ const router = require('./profile');
 router.route('/count').get(getTotalCount) //use to generate invoice serial number
 router.route('/:id').get(getInvoice).patch(updateInvoice).delete(deleteInvoice)
 router.route('/').get(getInvoicesByUser).post(createInvoice)
+router.route('/create').post(createInvoice)
 
 module.exports = router;
 
