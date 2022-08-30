16b32a6e7882d90a63836f14b6e94e1ad2854519
Amit Joshi <amit.joshi@stellarinfo.com>
2022-08-30 13:59:45 +0530

    CORS Add

--- app.js
+++ app.js
@@ -13,7 +13,26 @@ process.on('uncaughtException', err => {
     process.exit(1);
 });
 
+
+
 connectDatabase();
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
 // Set up body parser
 app.use(bodyParser.urlencoded({ extended : true }));
 // Setup body parser
